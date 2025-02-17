ca944cf36fa5ed231ab4274925b6eeb8d3c8a600
ErikaPezzini <erikapezzini66@gmail.com>
2025-02-17 11:29:18 +0100

    feat: triangolo e test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Poligono rettangolo = new Rettangolo(5, 3);
+        Poligono triangolo = new Triangolo(3, 4, 5, 4);
 
         System.out.println(rettangolo);
+        System.out.println(triangolo);
     }
 }
--- src/Triangolo.java
+++ src/Triangolo.java
@@ -1,3 +1,29 @@
-public class Triangolo {
-    
+class Triangolo implements Poligono {
+    private double lato1;
+    private double lato2;
+    private double lato3;
+    private double altezza;
+
+    public Triangolo(double lato1, double lato2, double lato3, double altezza) {
+        this.lato1 = lato1;
+        this.lato2 = lato2;
+        this.lato3 = lato3;
+        this.altezza = altezza;
+    }
+
+    @Override
+    public double calcolaPerimetro() {
+        return lato1 + lato2 + lato3;
+    }
+
+    @Override
+    public double calcolaArea(){
+        double base = lato1;
+        return (base * altezza) / 2;
+    }
+
+    @Override
+    public String toString(){
+        return "Triangolo [lati = " + lato1 + ", " + lato2 + ", " + lato3 + ", altezza = " + altezza + ", perimetro = " + calcolaPerimetro() + ", area = " + calcolaArea() + "]";
+    }
 }
