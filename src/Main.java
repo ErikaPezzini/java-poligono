b22aefc85cfc7db8f3a12e49d8669864b0bafbbd
ErikaPezzini <erikapezzini66@gmail.com>
2025-02-17 11:20:42 +0100

    feat: rettangolo e test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Poligono rettangolo = new Rettangolo(5, 3);
+
+        System.out.println(rettangolo);
+    }
 }
--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -1,3 +1,24 @@
-public class Rettangolo {
-    
+class Rettangolo implements Poligono {
+    private double base;
+    private double altezza;
+
+    public Rettangolo(double base, double altezza){
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    @Override
+    public double calcolaPerimetro(){
+        return 2 * (base + altezza);
+    }
+
+    @Override
+    public double calcolaArea(){
+        return base * altezza;
+    }
+
+    @Override
+    public String toString(){
+        return "Rettangolo [base = " + base + ", altezza = " + altezza + ", perimetro = " + calcolaPerimetro() + ", area = " + calcolaArea() + "]";
+    }
 }
